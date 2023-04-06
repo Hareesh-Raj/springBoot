b1675318ab8930614c52006cc237eb41561977c1
Hareesh-Raj <125631814+Hareesh-Raj@users.noreply.github.com>
2023-04-06 10:28:08 +0530

    Review Comments exercise 2

--- WebProject/src/main/java/com/example/demo/User.java
+++ WebProject/src/main/java/com/example/demo/User.java
@@ -1,19 +1,26 @@
 package com.example.demo;
 
+/*
+ * User class is used to store the user details and to perform operations.
+ * */
 public class User {
-	int uid;
+	int userID;
 	String Username;
-	public int getUid() {
-		return uid;
+
+	public int getUserID() {
+		return userID;
 	}
-	public void setUid(int uid) {
-		this.uid = uid;
+
+	public void setUserID(int userID) {
+		this.userID = userID;
 	}
+
 	public String getUsername() {
 		return Username;
 	}
+
 	public void setUsername(String username) {
 		Username = username;
 	}
-	
+
 }
--- WebProject/src/main/java/com/example/demo/WebProjectApplication.java
+++ WebProject/src/main/java/com/example/demo/WebProjectApplication.java
@@ -1,18 +1,16 @@
 package com.example.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.ApplicationContext;
 
-
+/*
+ * @author Hareesh Raj Ramanathan
+ * */
 @SpringBootApplication
 public class WebProjectApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(WebProjectApplication.class, args);
+		SpringApplication.run(WebProjectApplication.class, args);
 	}
 
 }
